49649504debdaa60e7d15b0f29517a11869143ca
plinioaugusto01 <87446879+plinioaugusto01@users.noreply.github.com>
2025-11-14 22:52:28 +0000

    alterado o TodoController

--- app/src/main/java/com/dynatrace/todoapp/TodoController.java
+++ app/src/main/java/com/dynatrace/todoapp/TodoController.java
@@ -76,12 +76,12 @@ public class TodoController {
         logger.debug("reading todoStore from database");
         logger.debug("SELECT * FROM todos WHERE status='conpleted'");
         // The bug in here in is for the bughunt example
-        List<TodoRecord> todoStore = new ArrayList<>();
-        logger.debug("todoStore size is {}", todoStore.size());
+        //List<TodoRecord> todoStore = new ArrayList<>();
+        //logger.debug("todoStore size is {}", todoStore.size());
         for (TodoRecord todoRecord : todos.getAll()) {
             if (todoRecord.isCompleted()) {
                 // The bug in here in is for the bughunt example
-                if (todoStore.remove(todoRecord)) {
+                if (todos.remove(todoRecord)) {
                     logger.info("Removing Todo record: {}", todoRecord);
                 }
             }
